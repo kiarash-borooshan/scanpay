e48711dca8de8d99a29dfb47748560923dd336a0
kia <K@b.com>
2023-12-21 12:13:22 +0330

    free css template initiated

--- sPay/store_warehouse/url.py
+++ sPay/store_warehouse/url.py
@@ -4,6 +4,7 @@ from django.urls import path
 app_name = "store_warehouse"
 
 urlpatterns = [
+    path("", views.first_page, name="first_page"),
     path("post_list/", views.post_list, name="post_list"),
     path("post_detail/<int:id_num>/", views.post_detail, name="post_detail")
 ]
--- sPay/store_warehouse/views.py
+++ sPay/store_warehouse/views.py
@@ -1,12 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Store
+
+
+def first_page(request):
+    return render(request,
+                  "store_warehouse/first_page.html")
 
 
 def post_list(request):
     # list = get_object_or_404(Store)
     stor_list = Store.objects.all()
     return render(request,
-                  "store_warehouse/list.html",
+                  "store_warehouse/post_list.html",
                   {"list": stor_list})
 
 
