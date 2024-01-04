c6f417d79c4da5a081cc83176d689e19443d0329
kia <K@b.com>
2024-01-04 16:39:00 +0330

    logout done

--- sPay/sPay/urls.py
+++ sPay/sPay/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("", include("store_warehouse.url")),
     path("spatial_services/", include("spatial_services.url")),
     path("social_media/", include("social_media.url")),
+    path("accounts/", include("Account.url")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/", include("Account.url"))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
