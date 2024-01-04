cf9b174eb3396c5d9b23c8efb72d780b07dd836b
kia <K@b.com>
2024-01-04 10:46:08 +0330

    logout failed

--- sPay/Account/url.py
+++ sPay/Account/url.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.contrib.auth import logout
+
 from . import views
 from django.urls import path
 
@@ -5,6 +8,7 @@ app_name = "Account"
 
 urlpatterns = [
     path("register/", views.Register.as_view(), name="register"),
+    # path("logout/", logout, {"next_page": settings.LOGOUT_REDIRECT_URL}, name="logout"),
     path("logout/", views.logout_view, name="logout"),
 ]
 
